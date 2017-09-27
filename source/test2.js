5062530d278faf1f4f41e6bec44546913be478fe
codecosine <846400886@qq.com>
2017-09-27 16:04:38 +0800

    目录改变

--- source/test2.js
+++ source/test2.js
@@ -1,25 +1,7 @@
-var v1 = []
-var v2 = {};
-var v3 = {};
-function foo(v1, v2, v3)
-{
-    v1 = [1];
-    v2 = [2];
-    v3 = {a:3}
+function arrayMerge(){
+    return [...arguments].reduce((result,value)=>{
+        return result.concat(value)
+    },[])
 }
-
-foo(v1, v2, v3);
-console.log(v1);
-console.log(v2);
-console.log(v3);
-
-
-
-function setName(obj){
-    obj.name="ted";
-    obj=new Object();
-    obj.name="marry";
-}
-var obj=new Object();
-setName(obj);
-alert(obj.name);
+var test = arrayMerge([1,2,3],[4,5],[10,200],[5327532895,23,3,4,4],["???",123])
+console.log(test)
